ac567d8011dc649e0f81ba8e027a99fc27ba229c
Desmond Parker <evan.parker95@gmail.com>
2021-12-07 23:04:45 +0000

    adding urls_new for letting users submit new webpages, and incorporated it into express_server, along with a function for generating random strings for shortURL

--- express_server.js
+++ express_server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function generateRandomString() {
+  let code = ''
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  for (let i = 0; i < 6; i++) {
+    code += chars.charAt(Math.floor(Math.random() * chars.length))
+  }
+  return code
+}
+
 
 app.set("view engine", "ejs");
 
@@ -22,6 +34,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: (urlDatabase[req.params.shortURL]) };
   res.render("urls_show", templateVars);
@@ -29,6 +45,11 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
